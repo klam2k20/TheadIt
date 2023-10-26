08bb36f3489b65dcc6825983f1ec0c5f1b9f27fb
klam2k20 <klam2k17@gmail.com>
2023-10-26 18:31:13 -0400

    Stop event propogation for modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -21,7 +21,10 @@ const Modal: React.FC<IModalProps> = ({ children, className }) => {
           className,
         )}
       >
-        <div className="relative h-fit w-full rounded-lg bg-white px-4 pb-4 pt-8">
+        <div
+          className="relative h-fit w-full rounded-lg bg-white px-4 pb-4 pt-8"
+          onClick={(event) => event.stopPropagation()}
+        >
           <CloseModalButton className="absolute right-2 top-2 h-8 w-8 rounded-md p-1" />
           {children}
         </div>
