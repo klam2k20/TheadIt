ee2acdef0f5d9b77ca7defb61b67f5eb75369060
klam2k20 <klam2k17@gmail.com>
2023-10-28 19:02:58 -0400

    Update use-login-toast to use ToastAction

--- src/hooks/use-login-toast.tsx
+++ src/hooks/use-login-toast.tsx
@@ -1,22 +1,23 @@
 import { buttonVariants } from "@/components/ui/Button";
-import { cn } from "@/lib/utils";
+import { ToastAction } from "@/components/ui/Toast";
 import Link from "next/link";
 import { toast } from "./use-toast";
 
 export const useLoginToast = () => {
   const loginToast = () => {
-    const { dismiss } = toast({
+    toast({
       title: "Login required.",
       description: "Please sign in to continue.",
       variant: "destructive",
       action: (
-        <Link
-          onClick={() => dismiss()}
-          href="/sign-in"
-          className={cn("text-black", buttonVariants({ variant: "outline" }))}
-        >
-          Sign In
-        </Link>
+        <ToastAction altText="Navigate to sign in" asChild>
+          <Link
+            href="/sign-in"
+            className={buttonVariants({ variant: "default" })}
+          >
+            Sign In
+          </Link>
+        </ToastAction>
       ),
     });
   };
