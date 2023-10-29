cd32a8d79cd23617006748df99c4a74f3c11807d
klam2k20 <klam2k17@gmail.com>
2023-10-28 20:30:02 -0400

    Update SignUp Modal to use Modal

--- src/app/@authModal/(.)sign-up/page.tsx
+++ src/app/@authModal/(.)sign-up/page.tsx
@@ -1,16 +1,11 @@
-import CloseModalButton from "@/components/CloseModalButton";
+import Modal from "@/components/Modal";
 import SignInBox from "@/components/SignInBox";
 
 const page = () => {
   return (
-    <div className="fixed inset-0 z-10 bg-zinc-900/20">
-      <div className="container flex h-full max-w-lg items-center justify-center">
-        <div className="relative h-fit w-full rounded-lg bg-white px-4 pb-4 pt-8">
-          <CloseModalButton className="absolute right-2 top-2 h-8 w-8 rounded-md p-1" />
-          <SignInBox isSignIn={false} />
-        </div>
-      </div>
-    </div>
+    <Modal>
+      <SignInBox isSignIn={false} />
+    </Modal>
   );
 };
 
