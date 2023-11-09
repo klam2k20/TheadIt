4d6b977138c7561372228f35b2f60d378bf9cd84
klam2k20 <klam2k17@gmail.com>
2023-11-09 15:52:30 -0500

    Validate that pathname is non-null in modal layout

--- src/app/@authModal/layout.tsx
+++ src/app/@authModal/layout.tsx
@@ -4,7 +4,8 @@ import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  return pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up")
+  return pathname &&
+    (pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up"))
     ? children
     : null;
 }
--- src/app/@createCommunityModal/layout.tsx
+++ src/app/@createCommunityModal/layout.tsx
@@ -4,5 +4,5 @@ import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  return pathname.startsWith("/t/create") ? children : null;
+  return pathname && pathname.startsWith("/t/create") ? children : null;
 }
